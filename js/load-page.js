c821ab3f6094dd31681d4c531c68c9fbfbfa289a
ALEX JUNIOR <85908664+AlexPelloso@users.noreply.github.com>
2021-08-23 22:56:57 +0000

    Commit- organizando saidas

--- js/load-page.js
+++ js/load-page.js
@@ -55,6 +55,35 @@
                 $('#modal-info').modal('show')
             break
 
+            //Se for para abrir a página ler mais rocket...
+            case 'rocket': 
+                $('.modal-title').append('Rocket League')
+                $('.modal-body').append(`<p>
+                <b>Mais informacões sobre o jogo:</b> <br><br>
+                Rocket League, híbrido de alta potência de futebol no estilo arcade e caos veicular da Psyonix, se tornou Free-to-play em todas as plataformas nesta quarta-feira (23).<br>
+                 Com a transição para o modelo gratuito, o game também estreou oficialmente na Epic Games Store e foi removido do Steam, a pedido da própria desenvolvedora. Com as mudanças, Rocket League também atingiu a marca de 1 milhão de jogadores conectados simultaneamente, como foi revelado por um dos chefões do estúdio via Twitter.<br>
+                 A desenvolvedora também anunciou uma promoção especial para aqueles jogadores que baixarem Rocket League através da Epic Games Store. Todos os usuários que fizerem o download através da plataforma até 23 de outubro serão contemplados com um cupom de R$ 40 para utilizar na loja. Será permitido apenas um cupom por conta, sendo ele intransferível e aplicado diretamente na próxima compra feita pelo jogador. O desconto gerado pelo cupom ficará válido até o dia 1 de novembro e só funcionará para compras acima de R$ 59,99. <br>
+
+                Para comemorar o lançamento Free-to-play, Rocket League vai receber de 26 de setembro a 12 de outubro o evento Llama-Rama. Durante o período, os jogadores poderão desbloquear itens do evento ao completar Desafios. Ao concluir todos os Desafios, os jogadores poderão ser recompensados com o mais novo carro do jogo, o ônibus de Batalha. <br>
+
+                Rocket League é um híbrido de alta potência de futebol no estilo arcade e caos veicular, com controles fáceis de entender e competição fluida e baseada em física. Rocket League está disponível gratuitamente no PC via Epic Games Store, Playstation 4, Xbox One e Nintendo Switch.
+                </p>`)
+                $('#modal-info').modal('show')
+            break
+
+            //Se for para abrir a página ler mais rocket...
+            case 'vava': 
+                $('.modal-title').append('Valorant')
+                $('.modal-body').append(`<p>
+                <b>Mais informacões sobre o jogo:</b> <br><br>
+                Já sabemos que a Havan Liberty será o primeiro dos dois representantes do Brasil no VALORANT Masters Berlin, que acontecerá entre os dias 10 e 19 de setembro, na capital alemã. Aproveitando o momento decisivo do VCT Challengers, com a Final Brasileira, gostaríamos de relembrar os próximos passos do cenário competitivo até chegar o momento mais esperado do ano por todos os fãs: a disputa do Champions, o grande Mundial do game.<br>
+                A grande decisão da Etapa 3 do Challengers Brazil acontece no próximo domingo (22 de agosto) e reunirá Havan Liberty contra uma das cinco outras equipes remanescentes na Chave Inferior do torneio. Nesta sexta, Vivo Keyd x YNG Sharks e Team Vikings x Stars Horizon fazem duelos eliminatórios. Os dois times que vencerem, avançam e se encaram para ver quem enfrenta a FURIA, valendo a vaga na Grande Final - e também em Berlim.<br>
+
+                Porém, é necessário relembrar: em outubro, antes do Champions, teremos um encontro entre times do Brasil e da América Latina valendo mais uma vaga para o Mundial, o Last Chance Qualifier. Ou seja: além das duas equipes nacionais que estarão garantidas pelos Pontos de Circuito, teremos chance de contar com um terceiro representante (que se transformaria em quarto, caso uma organização brasileira conquiste o Masters 3).
+                 </p>`)
+                $('#modal-info').modal('show')
+            break
+
             //Se for para abrir a página Whatsapp...
             case 'whats': 
                 $('.modal-title').append('Whatsapp')
